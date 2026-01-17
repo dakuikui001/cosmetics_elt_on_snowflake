6f722b0915fd18f70c1bce4440ea7a80d3813546
dakuikui001 <hui011@e.ntu.edu.sg>
2026-01-17 18:48:26 +0800

    refactor: add path filtering for infra SQL and bypass logic

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,4 +5,7 @@ resource "snowflake_database" "cosmetics_db" {
 resource "snowflake_schema" "cosmetics_schema" {
   database = snowflake_database.cosmetics_db.name
   name     = "COSMETICS"
+  is_transient        = false
+  with_managed_access = false
+  data_retention_time_in_days = 1
 }
